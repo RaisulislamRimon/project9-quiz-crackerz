33f053258445802fdd736c44721cfaa7bec649fc
MD. Raisul Islam Rimon <rimonppip0@gmail.com>
2022-10-11 19:09:44 +0600

    added hero icons in home page

--- src/components/AllTopics/AllTopics.js
+++ src/components/AllTopics/AllTopics.js
@@ -4,7 +4,7 @@ import SingleTopic from "../SingleTopic/SingleTopic";
 const AllTopics = ({ quiz }) => {
   return (
     <div>
-      <h1 className="text-4xl text-center my-5 p-10 md:p-8">
+      <h1 className="text-4xl md:font-bold md:text-cyan-400 lg:text-black text-center my-5 p-10 md:p-8">
         Take a quiz and get a chance to get your dream job.
       </h1>
       <div className="container mx-auto grid md:grid-cols-1 lg:grid-cols-2 ">
--- src/components/SingleTopic/SingleTopic.js
+++ src/components/SingleTopic/SingleTopic.js
@@ -9,10 +9,28 @@ const SingleTopic = ({ singleQuiz }) => {
           <img src={logo} alt={name} />
         </figure>
         <div className="card-body">
-          <h2 className="card-title">{name}</h2>
+          <h2 className="card-title text-3xl">{name}</h2>
           <p>Total questions : {total}</p>
           <div className="card-actions justify-end">
-            <button className="btn btn-primary">Start Now</button>
+            <button className="btn btn-primary">
+              Start Now
+              <span>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="w-6 h-6"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M4.5 12h15m0 0l-6.75-6.75M19.5 12l-6.75 6.75"
+                  />
+                </svg>
+              </span>
+            </button>
           </div>
         </div>
       </div>
