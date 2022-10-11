65466b319f9927cec1f9071183fe4725684e1ebc
MD. Raisul Islam Rimon <rimonppip0@gmail.com>
2022-10-11 18:53:15 +0600

    update SingleTopic card & showing details

--- src/components/SingleTopic/SingleTopic.js
+++ src/components/SingleTopic/SingleTopic.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const SingleTopic = ({ singleQuiz }) => {
-  const { logo, name } = singleQuiz;
+  const { logo, name, total } = singleQuiz;
   return (
     <div className="mt-16  mx-auto">
       <div className="card w-96 bg-base-100 shadow-xl">
@@ -10,9 +10,9 @@ const SingleTopic = ({ singleQuiz }) => {
         </figure>
         <div className="card-body">
           <h2 className="card-title">{name}</h2>
-          <p>If a dog chews shoes whose shoes does he choose?</p>
+          <p>Total questions : {total}</p>
           <div className="card-actions justify-end">
-            <button className="btn btn-primary">Buy Now</button>
+            <button className="btn btn-primary">Start Now</button>
           </div>
         </div>
       </div>
