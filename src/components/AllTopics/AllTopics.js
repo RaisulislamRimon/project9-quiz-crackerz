37b3aadf6e1384494fa57bee246d4568fb851631
MD. Raisul Islam Rimon <rimonppip0@gmail.com>
2022-10-11 18:35:47 +0600

    added title in home page

--- src/components/AllTopics/AllTopics.js
+++ src/components/AllTopics/AllTopics.js
@@ -3,16 +3,21 @@ import SingleTopic from "../SingleTopic/SingleTopic";
 
 const AllTopics = ({ quiz }) => {
   return (
-    <div className="container mx-auto grid md:grid-cols-1 lg:grid-cols-2 ">
-      {
-        // quiz.data.map(singleQuiz => <h1 key={singleQuiz.id}>{singleQuiz.name}</h1>)
-        quiz.data.map((singleQuiz) => (
-          <SingleTopic
-            key={singleQuiz.id}
-            singleQuiz={singleQuiz}
-          ></SingleTopic>
-        ))
-      }
+    <div>
+      <h1 className="text-4xl text-center my-5 p-10 md:p-8">
+        Take a quiz and get a chance to get your dream job.
+      </h1>
+      <div className="container mx-auto grid md:grid-cols-1 lg:grid-cols-2 ">
+        {
+          // quiz.data.map(singleQuiz => <h1 key={singleQuiz.id}>{singleQuiz.name}</h1>)
+          quiz.data.map((singleQuiz) => (
+            <SingleTopic
+              key={singleQuiz.id}
+              singleQuiz={singleQuiz}
+            ></SingleTopic>
+          ))
+        }
+      </div>
     </div>
   );
 };
