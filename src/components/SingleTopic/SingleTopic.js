e272ade26ba2388b0304a1956baa6022dbcd1e8e
MD. Raisul Islam Rimon <rimonppip0@gmail.com>
2022-10-11 18:39:50 +0600

    added img, title of single quiz in home page

--- src/components/SingleTopic/SingleTopic.js
+++ src/components/SingleTopic/SingleTopic.js
@@ -1,15 +1,15 @@
 import React from "react";
 
 const SingleTopic = ({ singleQuiz }) => {
-  console.log(singleQuiz);
+  const { logo, name } = singleQuiz;
   return (
     <div className="mt-16  mx-auto">
       <div className="card w-96 bg-base-100 shadow-xl">
         <figure>
-          <img src="https://placeimg.com/400/225/arch" alt="Shoes" />
+          <img src={logo} alt={name} />
         </figure>
         <div className="card-body">
-          <h2 className="card-title">Shoes!</h2>
+          <h2 className="card-title">{name}</h2>
           <p>If a dog chews shoes whose shoes does he choose?</p>
           <div className="card-actions justify-end">
             <button className="btn btn-primary">Buy Now</button>
