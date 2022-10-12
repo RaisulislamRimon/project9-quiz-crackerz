9674355281cb622b03a2a79a97aa6042ecd96a67
MD. Raisul Islam Rimon <rimonppip0@gmail.com>
2022-10-12 09:11:12 +0600

    update SingleQuiz card

--- src/components/SingleQuiz/SingleQuiz.js
+++ src/components/SingleQuiz/SingleQuiz.js
@@ -8,7 +8,6 @@ const SingleQuiz = (props) => {
       <div className="card w-full bg-base-100 shadow-xl">
         <div className="card-body">
           <h2 className="card-title">Quiz : {question}</h2>
-          <p>If a dog chews shoes whose shoes does he choose?</p>
           <div className="card-actions justify-end">
             <button className="btn btn-outline btn-primary">Submit</button>
           </div>
