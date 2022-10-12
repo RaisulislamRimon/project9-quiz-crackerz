11339bd6a9bd9a813fa536d43eb1bec9f517c2a6
MD. Raisul Islam Rimon <rimonppip0@gmail.com>
2022-10-12 23:03:12 +0600

    added Footer

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="footer footer-center p-4 mb-3 pt-10  bg-base-300 text-base-content sticky top-[100vh]">
+      <div>
+        <p className="text-lg">
+          Copyright © 2022 - All right reserved by MD. Raisul Islam Rimon
+        </p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
--- src/layouts/Main.js
+++ src/layouts/Main.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
+import Footer from "../components/Footer/Footer";
 import Header from "../components/Header/Header";
 
 const Main = () => {
   return (
-    <div>
+    <div className="min-h-screen">
       <Header />
       <Outlet />
+      <Footer />
     </div>
   );
 };
